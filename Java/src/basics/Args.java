463a1c833f89b1381a61e84669f06c4a94c01731
Shushant <profshushant@gmail.com>
2025-01-19 23:42:33 +0530

    learned about cmd args

---
+++ Java/src/basics/Args.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class Args {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print(args[0]);
+        System.out.print(args[1]);
+    }
+}
