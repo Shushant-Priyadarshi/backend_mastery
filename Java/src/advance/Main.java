1d00fad6269fde99f53806e21767f6298446c823
Shushant <profshushant@gmail.com>
2025-01-27 17:55:37 +0530

    advance topics done

--- Java/src/advance/Main.java
+++ Java/src/advance/Main.java
@@ -5,6 +5,15 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        HashMap<String ,Integer> marks = new HashMap<>();
+        marks.put("Rajneet",90);
+        marks.put("Shushant",80);
+        System.out.println(marks);
+        System.out.println(marks.get("Rajneet"));
 
+        for(Map.Entry<String ,Integer> entry : marks.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+            System.out.println("********************************************");
+        }
     }
 }
