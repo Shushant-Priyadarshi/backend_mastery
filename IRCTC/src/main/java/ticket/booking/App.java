d8cea5758c24b877e131252238df30ed4e4b8759
Shushant <profshushant@gmail.com>
2025-01-31 17:22:24 +0530

    almost done

--- IRCTC/src/main/java/ticket/booking/App.java
+++ IRCTC/src/main/java/ticket/booking/App.java
@@ -1,9 +1,23 @@
 package ticket.booking;
 
+import ticket.booking.service.UserBooking;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class App {
 
 
     public static void main(String[] args) {
-
+        System.out.println("***************** Running Train Booking Application *****************");
+        Scanner scanner = new Scanner(System.in);
+        int option=0;
+        UserBooking  userBookingService;
+        try{
+            userBookingService = new UserBooking();
+        }catch (IOException e){
+            System.out.println("There was something went wrong!!");
+            return;
+        }
     }
 }
--- IRCTC/src/main/java/ticket/booking/service/UserBooking.java
+++ IRCTC/src/main/java/ticket/booking/service/UserBooking.java
@@ -3,10 +3,12 @@ package ticket.booking.service;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBooking {
 
@@ -14,12 +16,50 @@ public class UserBooking {
     private List<User> userList;
 
     private ObjectMapper objectMapper = new ObjectMapper();
-    private static final String USERS_PATH = "../db/users.json";
+    private static final String USERS_PATH = "src/main/java/ticket/booking/db/users.json";
 
     public UserBooking(User user) throws IOException {
         this.user = user;
+        loadUsers();
+    }
+
+    public UserBooking() throws IOException {
+        loadUsers();
+    }
+
+    public List<User> loadUsers()  throws IOException {
         File users = new File(USERS_PATH);
-        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+        return userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    }
+
+    public boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName()
+                    .equals(user.getName()) &&
+                    UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
 
     }
+
+    public boolean signUp(User user){
+        try{
+            userList.add(user);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch(IOException e){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File userFile = new File(USERS_PATH);
+        objectMapper.writeValue(userFile, userList);
+    }
+
+    public void fetchBooking(){
+        user.printTickets();
+    }
+
+
 }
