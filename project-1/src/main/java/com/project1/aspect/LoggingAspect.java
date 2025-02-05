a9e405b5cd198eb0d978eb18a12cf27bc39d76b9
Shushant <profshushant@gmail.com>
2025-02-05 23:11:29 +0530

    aop complete

--- project-1/src/main/java/com/project1/aspect/LoggingAspect.java
+++ project-1/src/main/java/com/project1/aspect/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.project1.aspect;
 
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,7 +12,12 @@ public class LoggingAspect {
 
     @Before("execution(* com.project1.service.PaymentServiceImpl.makePayment())")
     public void logBefore() {
-        System.out.println("Payment is getting executed....");
+        System.out.println("Payment mode is getting enabled....");
+    }
+
+    @After("execution(* com.project1.service.PaymentServiceImpl.makePayment())")
+    public void logAfter() {
+        System.out.println("Payment mode is getting disabled....");
     }
 
 }
